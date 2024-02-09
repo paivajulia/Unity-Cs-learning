e10fe0fa3a3dd9564dda94e46dee21dfe4758a48
paivajulia <paivafreitas.julia@gmail.com>
2024-02-09 11:14:52 -0500

    Update LogicScript

--- Assets/CatScript.cs
+++ Assets/CatScript.cs
@@ -7,20 +7,29 @@ public class CatScript : MonoBehaviour
 
     public Rigidbody2D myRigidbody2D;
     public float flapStrength;
+    public LogicScript logic;
+    public bool catIsAlive = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && catIsAlive == true)
         {
             myRigidbody2D.velocity = Vector2.up * flapStrength; 
         }
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -14,5 +17,16 @@ public class LogicScript : MonoBehaviour
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
 
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
